5164b87b880a7f6137f719166bfc5b13e110481c
TimingJL <eefozeo@yahoo.com.tw>
2016-07-11 22:51:29 +0800

    ActiveRecord basic operation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,8 @@
 class Event < ApplicationRecord
+	has_many :attendees # 複數
+	belongs_to :category
+	has_one :location, :dependent => :destroy
+    has_many :event_groupships
+    has_many :groups, :through => :event_groupships	
+    has_many :attendees, ->{ where(["created_at > ?", Time.now - 7.day]).order("id DESC") }, :dependent => :destroy
 end
