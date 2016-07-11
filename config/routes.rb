cc54b5f692a39b3e3a44748a009b13e398cd7153
TimingJL <eefozeo@yahoo.com.tw>
2016-07-11 20:40:11 +0800

    modify to RESTful CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  resources :people
+	resources :events
+	resources :people
 	get "welcome/say_hello" => "welcome#say"
 	get "welcome" => "welcome#index"
 
 	root :to => "welcome#index"
-	match ':controller(/:action(/:id(.:format)))', :via => :all
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	#match ':controller(/:action(/:id(.:format)))', :via => :all
+	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
