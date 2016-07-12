a78f3f51a71e0b297295c88542c6b36cd4148c0c
TimingJL <eefozeo@yahoo.com.tw>
2016-07-12 21:46:46 +0800

    RESTful Application Example2: Pull-down menus of Category

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,4 +5,5 @@ class Event < ApplicationRecord
     has_many :event_groupships
     has_many :groups, :through => :event_groupships	
     has_many :attendees, ->{ where(["created_at > ?", Time.now - 7.day]).order("id DESC") }, :dependent => :destroy
+
 end
