cd197bfbe619f033a0ea8c6311184efdf2c5d959
TimingJL <eefozeo@yahoo.com.tw>
2016-07-12 23:02:42 +0800

    RESTful Application: Using Nested Model to Edit and New

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,5 +5,5 @@ class Event < ApplicationRecord
     has_many :event_groupships
     has_many :groups, :through => :event_groupships	
     has_many :attendees, ->{ where(["created_at > ?", Time.now - 7.day]).order("id DESC") }, :dependent => :destroy
-
+	accepts_nested_attributes_for :location, :allow_destroy => true, :reject_if => :all_blank
 end
